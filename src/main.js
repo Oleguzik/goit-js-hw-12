2c68605ace8b655530ec363235f015d8c012d9ad
Oleguzik <guzik.oleg@gmail.com>
2024-01-20 16:27:38 +0100

    working version

--- src/main.js
+++ src/main.js
@@ -4,72 +4,136 @@ import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
+import axios from 'axios';
+
 const searchForm = document.querySelector('.search');
 const gallery = document.querySelector('.gallery');
+const moreBtn = document.querySelector('.moreBtn');
 const textInput = document.querySelector('.textInput');
+const loader = document.querySelector('.loader');
 const modal = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
 });
 
-const loader = document.querySelector('.loader');
+const perPage = 40;
+
+let currentPage = 1;
+let queryValue = ''; // current value of the search
 
 searchForm.addEventListener('submit', onSearchSubmit);
+moreBtn.addEventListener('click', onMoreSubmit);
 
-function onSearchSubmit(e) {
-  e.preventDefault();
-  const value = e.target.elements.search.value;
+function onSearchSubmit(e) { // functions on search submit for search form
+  e.preventDefault(); // cancels the event on search submit
+  queryValue = e.target.elements.search.value; // get the value of the search
   gallery.innerHTML = '';
   textInput.value = '';
-  loader.style.display = 'block';
-  fetchData(formQueryUrl(value));
+  loader.style.visibility = 'visible';
+  currentPage = 1;
+  fetchDataBtn(formQueryUrl(queryValue, currentPage));
 }
 
-function formQueryUrl(v) {
+function onMoreSubmit(e) { // function on more submit button
+  moreBtn.style.visibility = 'hidden'; // more button is hidden
+  loader.style.visibility = 'visible'; // loader is visible
+  currentPage += 1; // increment the current page
+  moreDataBtn(formQueryUrl(queryValue, currentPage));
+}
+
+function formQueryUrl(value, page) { // form url with query value
   const BASE_URL = 'https://pixabay.com/api';
   const OPTIONS = new URLSearchParams({
     orientation: 'horizontal',
     image_type: 'photo',
     safesearch: 'true',
     key: '29710136-0ffe1ca247b000977a61f6ae2',
+    per_page: perPage,
+    page,
   });
-  return `${BASE_URL}/?q=${v}&${OPTIONS.toString()}`;
+  return `${BASE_URL}/?q=${value}&${OPTIONS.toString()}`;
 }
 
-function fetchData(requestData) {
-  fetch(requestData)
-    .then(response => {
-      loader.style.display = 'none';
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    })
-    .then(data => {
-      if (data.hits.length === 0) {
-        iziToast.error({
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-          messageColor: '#FAFAFB',
-          backgroundColor: '#EF4040',
-          position: 'topRight',
-        });
-        return;
-      }
-      const imagesHTML = data.hits.reduce((html, image) => {
-        return html + imageCard(image);
-      }, '');
-
-      gallery.innerHTML = imagesHTML;
-      modal.refresh();
-      
-    })
-    .catch(error => {
-      alert(error.toString());
-    });
+async function fetchDataBtn(requestData) { // fetch data, form result and resulting page
+  try {
+    currentPage = 1;
+    const response = await axios.get(requestData); // fetch data
+    loader.style.visibility = 'hidden'; // loader is hidden
+    if (response.status !== 200) {
+      myAlert(response.status); // show error message from server
+    }
+    if (response.data.hits.length === 0) { // no hits from server
+      myAlert(
+        'Sorry, there are no images matching your search query. Please try again!'
+      );
+      return;
+    }
+    const imagesHTML = response.data.hits.reduce((html, image) => {
+      return html + imageRender(image);
+    }, '');
+    gallery.insertAdjacentHTML('beforeend', imagesHTML); // add images set to gallery
+    modal.refresh(); // refresh SimpleLightbox
+    if (response.data.totalHits > currentPage * 40) { // check if out of total page hits
+      moreBtn.style.visibility = 'visible'; // show more button
+    } else {
+      moreBtn.style.visibility = 'hidden'; // hide more button
+      myAlert("We're sorry, but you've reached the end of search results.");
+    }
+  } catch (error) {
+    myAlert(error.toString()); // show more error message
+  }
 }
 
-function imageCard(images) {
+async function moreDataBtn(requestData) { // button for more search results
+  try {
+    const response = await axios.get(requestData);
+    loader.style.visibility = 'hidden'; // loader is hidden
+    if (response.status !== 200) {
+      myAlert(response.status); // show error message from server
+    }
+    if (response.data.hits.length === 0) {
+      myAlert(
+        'Sorry, there are no images matching your search query. Please try again!'
+      );
+      return;
+    }
+    const imagesHTML = response.data.hits.reduce((html, image) => {
+      return html + imageRender(image);
+    }, '');
+    gallery.insertAdjacentHTML('beforeend', imagesHTML); // add images set to gallery
+    modal.refresh(); // refresh SimpleLightbox
+    if (response.data.totalHits > currentPage * 40) {
+      moreBtn.style.visibility = 'visible';
+      setScrolling(); // scroll to show more images
+    } else {
+      moreBtn.style.visibility = 'hidden';
+      moreBtn.removeEventListener('click', onMoreSubmit) // remove event listener for more button to release resources 
+      myAlert("We're sorry, but you've reached the end of search results.");
+    }
+  } catch (error) {
+    myAlert(error.toString()); // show more error message
+  }
+}
+
+function setScrolling() { // set scrolling gallery
+  window.scrollBy({
+    top: gallery.getBoundingClientRect().height * 0.04,
+    behavior: 'smooth',
+  });
+}
+
+function myAlert(message) { // alert message in iziToast form
+  iziToast.error({
+    message: message,
+    messageColor: '#FAFAFB',
+    backgroundColor: '#EF4040',
+    position: 'topRight',
+    transitionIn: 'flipInX',
+    transitionOut: 'flipOutX',
+  });
+}
+
+function imageRender(images) { // render fetched images
   return `<li>
         <a href="${images.largeImageURL}">
           <img src="${images.webformatURL}" alt="${images.tags}">
